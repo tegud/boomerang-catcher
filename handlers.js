992f7640af7a0410741731c9f2c5cd6fc446ef84
Steve Elliott <steve@tegud.net>
2019-03-23 08:30:14 +0000

    simple http endpoint /w CORS support

---
+++ handlers.js
@@ -0,0 +1,20 @@
+const getResponseHeaders = () => {
+  if (!process.env.corsAllowOrigin) {
+    return {};
+  }
+
+  return {
+    'Access-Control-Allow-Origin': process.env.corsAllowOrigin;
+  };
+};
+
+module.exports.beacon = async (event) => {
+  const parameters = Object.entries(event.queryStringParameters);
+
+  console.log(parameters);
+
+  return {
+    statusCode: 200,
+    headers: getHeaders(),
+  };
+};
